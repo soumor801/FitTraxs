66a5bc9bca8c718a6287eca0a094d96e5e17bce3
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-10-22 16:16:54 -0400

    add form to traxslog page

--- client/src/Components/TraxsLog.js
+++ client/src/Components/TraxsLog.js
@@ -4,12 +4,14 @@ function TraxsLog() {
     <div>
       <h1>TraxsLogs</h1>
       <form>
+      <label htmlFor="Name of WorkOut">Name of WorkOut: </label>
+        <input type='text' placeholder='Bench Press' id='Name of WorkOut'/> 
         
-        <input type='text' placeholder='Bench Press'/> 
-       
-        <input type='text' placeholder='10 Reps'/>
+        <label htmlFor="Number of Reps">Number of Reps: </label>
+        <input type='text' placeholder='10 Reps' id='Number of Reps' />
         
-        <input type='text' placeholder='3 Sets'/>
+        <label htmlFor="Number of Sets">Number of Sets: </label>
+        <input type='text' placeholder='3 Sets' id='Number of Sets'/>
         
 
 
