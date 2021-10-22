1b8d66b98162a5eaa2ddeb5fc7db16778fcab647
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-10-22 18:09:37 -0400

    traxlogs form functionality

--- client/src/Components/TraxsLog.js
+++ client/src/Components/TraxsLog.js
@@ -1,9 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const API_URL = 'https://api.airtable.com/v0/appApuXEvhttewhoa/Table%201'
 
 function TraxsLog() {
+  const [nameOfWorkout, setNameOfWorkOut] = useState('');
+  const [numsOfReps, setNumsOfReps] = useState('');
+  const [numsOfSets, setNumsOfSets] = useState('');
+
+const handlePostRequest = async (ev) => {
+  ev.preventDefault();
+  const newWorkOut= {
+    records: [
+      {
+        fields: {
+          'Workout Name': nameOfWorkout,
+          'reps': numsOfReps,
+          'sets': numsOfSets
+        }
+      }
+    ]
+  }
+
+  await axios.post(API_URL, newWorkOut);
+  
+  
+}
+
+  
+
   return (
     <div>
       <h1>TraxsLogs</h1>
-      <form>
+      <form onSubmit={handlePostRequest}>
       <label htmlFor="Name of WorkOut">Name of WorkOut: </label>
         <input type='text' placeholder='Bench Press' id='Name of WorkOut'/> 
         
@@ -12,7 +41,7 @@ function TraxsLog() {
         
         <label htmlFor="Number of Sets">Number of Sets: </label>
         <input type='text' placeholder='3 Sets' id='Number of Sets'/>
-        
+        <input type='submit'/>
 
 
 
