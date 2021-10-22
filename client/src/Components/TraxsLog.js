7510a7c24a7a73680f8ad2b808bbb859d90395a6
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-10-22 18:49:55 -0400

    traxpage functionality

--- client/src/Components/TraxsLog.js
+++ client/src/Components/TraxsLog.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
-const API_URL = 'https://api.airtable.com/v0/appApuXEvhttewhoa/Table%201'
+const API_URL = 'https://api.airtable.com/v0/appApuXEvhttewhoa/Table%201?api_key=keyzjTZF2QiihjSEs'
 
 function TraxsLog() {
   const [nameOfWorkout, setNameOfWorkOut] = useState('');
@@ -34,13 +34,13 @@ const handlePostRequest = async (ev) => {
       <h1>TraxsLogs</h1>
       <form onSubmit={handlePostRequest}>
       <label htmlFor="Name of WorkOut">Name of WorkOut: </label>
-        <input type='text' placeholder='Bench Press' id='Name of WorkOut'/> 
+        <input type='text' placeholder='Bench Press' id='Name of WorkOut'  onChange={(ev) => setNameOfWorkOut(ev.target.value)}/>
         
         <label htmlFor="Number of Reps">Number of Reps: </label>
-        <input type='text' placeholder='10 Reps' id='Number of Reps' />
+        <input type='text' placeholder='10 Reps' id='Number of Reps' onChange={(ev) => setNumsOfReps(ev.target.value)} />
         
         <label htmlFor="Number of Sets">Number of Sets: </label>
-        <input type='text' placeholder='3 Sets' id='Number of Sets'/>
+        <input type='text' placeholder='3 Sets' id='Number of Sets' onChange={(ev) => setNumsOfSets(ev.target.value)}/>
         <input type='submit'/>
 
 
--- client/src/Components/TraxsPage.js
+++ client/src/Components/TraxsPage.js
@@ -1,9 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const API_URL = 'https://api.airtable.com/v0/appApuXEvhttewhoa/Table%201?api_key=keyzjTZF2QiihjSEs'
 
 function TraxsPage() {
+  const [WorkoutData, setWorkoutData] = useState([]);
+  useEffect(() => {
+   
+
+    const getWorkoutData = async () => {
+      const resp = await axios.get(API_URL);
+      
+      setWorkoutData(resp.data.records);
+      console.log(setWorkoutData)
+    }
+
+    getWorkoutData();
+  }, []);
+
   return (
     <div>
-      <h1>TraxsPage</h1>
+      <h1>TraxsPage</h1>      
+      {WorkoutData.map((eachWorkout) => (
+        <div>
+          <h2> {eachWorkout.fields.WorkoutName}</h2>
+          <h4> Reps:{eachWorkout.fields.reps}</h4>
+          <h4>Sets:{eachWorkout.fields.sets}</h4>
+        
+        </div>
+      ))}
     </div>
   )
 }
