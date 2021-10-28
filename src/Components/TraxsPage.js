15687146ecca43c1e92af2e7d7d881bed4adb7b0
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-10-28 10:27:25 -0400

    clean up

--- src/Components/TraxsPage.js
+++ src/Components/TraxsPage.js
@@ -2,54 +2,54 @@ import axios from "axios";
 import { useEffect, useState, useParams } from "react";
 import TraxsLog from "./TraxsLog";
 
-const API_URL = 'https://api.airtable.com/v0/appApuXEvhttewhoa/Table%201?api_key=keyzjTZF2QiihjSEs'
+const API_URL =
+  "https://api.airtable.com/v0/appApuXEvhttewhoa/Table%201?api_key=keyzjTZF2QiihjSEs";
 
 function TraxsPage() {
   const [workoutData, setWorkoutData] = useState([]);
   const [toggleFetch, setToggleFetch] = useState(false);
   const params = useParams;
   useEffect(() => {
-   
-
     const getWorkoutData = async () => {
       const resp = await axios.get(API_URL);
-      
-      setWorkoutData(resp.data.records);
-      
-    }
 
+      setWorkoutData(resp.data.records);
+    };
     getWorkoutData();
-    console.log(workoutData)
+    console.log(workoutData);
   }, [toggleFetch]);
-  ;
-  // const workoutId = workoutData.records.id
-  // console.log(workoutId)
-  // console.log(workout_id)
   const deleteWorkout = async (id) => {
-    
-    
     const resp = await axios.delete(API_URL + `&records[]=${id}`);
-    setToggleFetch(!toggleFetch)
-  }
+    setToggleFetch(!toggleFetch);
+  };
 
   return (
     <div>
-      <h1 className="traxsHeader " >TraxsPage</h1>
+      <h1 className="traxsHeader ">TraxsPage</h1>
       <hr />
       {workoutData.map((eachWorkout) => (
         <div key={eachWorkout.id}>
           <section>
-          <h2 className="traxsHeader "> {eachWorkout.fields.WorkoutName}</h2>
-          <h4 className="traxsHeader "r> Reps:{eachWorkout.fields.reps}</h4>
-          <h4 className="traxsHeader ">Sets:{eachWorkout.fields.sets}</h4>
-          <button type='submit' className="btnPrimary " onClick={(id) => { deleteWorkout(eachWorkout.id) }}>Remove Workout</button>
+            <h2 className="traxsHeader "> {eachWorkout.fields.WorkoutName}</h2>
+            <h4 className="traxsHeader " r>
+              {" "}
+              Reps:{eachWorkout.fields.reps}
+            </h4>
+            <h4 className="traxsHeader ">Sets:{eachWorkout.fields.sets}</h4>
+            <button
+              type="submit"
+              className="btnPrimary "
+              onClick={(id) => {
+                deleteWorkout(eachWorkout.id);
+              }}
+            >
+              Remove Workout
+            </button>
           </section>
           <hr />
-          
         </div>
       ))}
-     
     </div>
-  )
+  );
 }
 export default TraxsPage;
